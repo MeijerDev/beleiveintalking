5c58b2a6b645f58064ce3d9bb813a40eea109191
Patserbak <36964214+Patserbak@users.noreply.github.com>
2023-07-24 16:56:34 +0100

    update
    update

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 CONTACT_ENDPOINT=https://6vepad8o23.execute-api.eu-central-1.amazonaws.com/default/ContactFormBelieveInTalking
 
-# rm -f ./src/*.js
 npx tsc ./src/index.ts --outFile ./src/index.js
-
 sed -i -e "s#CONTACT_ENDPOINT#$CONTACT_ENDPOINT#g" ./src/index.js
-# rm -f ./src/*.js-e
-
 npx @11ty/eleventy --output local
 
 # aws s3 sync --region eu-central-1 out "s3://meijer-m93.nl/" --exclude "*.DS_Store*" 
