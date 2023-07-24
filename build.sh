f92dfb397da946c2ec6531c186207d42752e635e
Patserbak <36964214+Patserbak@users.noreply.github.com>
2023-07-24 16:54:15 +0100

    update

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ npx tsc ./src/index.ts --outFile ./src/index.js
 sed -i -e "s#CONTACT_ENDPOINT#$CONTACT_ENDPOINT#g" ./src/index.js
 # rm -f ./src/*.js-e
 
-npx @11ty/eleventy
+npx @11ty/eleventy --output local
 
 # aws s3 sync --region eu-central-1 out "s3://meijer-m93.nl/" --exclude "*.DS_Store*" 
 # aws cloudfront create-invalidation --distribution-id E2O8C7EREJTLL3 --paths "/*"
