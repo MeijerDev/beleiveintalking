825752c2e1a773c95bef33f4aa8f00e1647570de
Patserbak <36964214+Patserbak@users.noreply.github.com>
2023-07-27 13:43:46 +0100

    update

--- .eleventy.js
+++ .eleventy.js
@@ -5,31 +5,10 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addCollection("pages", async () => {
 
         try {
-            var myHeaders = new Headers();
-            myHeaders.append("Authorization", "Basic " + Buffer.from("Cppio_voVO6Me3UPXIYwqQ..:a5O5oq8LntWtwtVLw2SBLw..").toString("base64"));
-            myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-
-            const urlencoded = new URLSearchParams();
-            urlencoded.append("grant_type", "client_credentials");
-
-            var answer = await fetch("https://hl7offzwezq2cal-db202103270929.adb.uk-london-1.oraclecloudapps.com/ords/api/oauth/token", {
-                method: "POST",
-                headers: myHeaders,
-                body: urlencoded,
-                redirect: "follow"
-            });
-            var result = await answer.json();
-            const token = result?.access_token;
-
-            myHeaders = new Headers();
-            myHeaders.append("Authorization", "Bearer " + token);
-
-            answer = await fetch("https://hl7offzwezq2cal-db202103270929.adb.uk-london-1.oraclecloudapps.com/ords/api/auth/content/believeintalking.com", {
+            answer = await fetch("https://hl7offzwezq2cal-db202103270929.adb.uk-london-1.oraclecloudapps.com/ords/api/public/content/believeintalking.com", {
                 method: 'GET',
-                headers: myHeaders,
             });
             result = await answer.json();
-            // console.log(result);
             return result;
         } catch (err) {
             console.log("error while fetching website parts: ", err);
